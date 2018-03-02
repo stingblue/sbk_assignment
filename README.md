bcc8655dee7732366454ef1c2379c39fa8c2f668
stingbue <thanh.vo>
2018-03-02 22:29:38 +0700

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# sbk_assignment
+Rules Check(HTML file)
+=========
+
+A small library that scan 1 html and output validation
+
+## Installation
+
+  `npm install @stingblue/rules-check`
+
+## Usage
+
+    var rulesCheck = require('@stingblue/rules-check');
+
+    var xxx = rulesCheck(---);
+  
+  
+  Output should be ``
+
+
+## Tests
+
+  `npm test`
+
+## Contributing
+
+In lieu of a formal style guide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code.
---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
+};
+/*
+console.log(2);
+
+
+
+function check_rule() {
+	
+}
+*/
+/*
+var fs = require('fs');
+var ws = fs.createWriteStream('message.txt');
+
+ws.write('beep ');
+
+setTimeout(function () {
+    ws.end('boop\n');
+}, 1000);
+
+*/
+/*
+const fs = require('fs');
+const rr = fs.createReadStream('message.txt');
+rr.on('readable', () => {
+  console.log(`readable: ${rr.read()}`);
+});
+rr.on('end', () => {
+  console.log('end');
+});
+*/
